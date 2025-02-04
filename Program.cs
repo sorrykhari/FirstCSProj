94e91305f2369f00eb0c23f0e4aa1914ff9ece45
Kai <kharimobley@gmail.com>
2025-02-03 21:19:48 -0500

    starting over bruh lol

--- Program.cs
+++ Program.cs
@@ -1,104 +1,14 @@
-﻿using FirstCSProj;
+﻿using System;
 
-/* Practice section
-Console.WriteLine("Enter the number of the month you were born (1-12): ");
-
-int month = int.Parse(Console.ReadLine());
-
-switch (month)
+namespace FirstCSProj
 {
-    case 1:
-        Console.WriteLine("January");
-        break;
-    case 2:
-        Console.WriteLine("Febraury");
-        break;
-    case 3:
-        Console.WriteLine("March");
-        break;
-    case 4:
-        Console.WriteLine("April");
-        break;
-    case 5:
-        Console.WriteLine("May");
-        break;
-    case 6:
-        Console.WriteLine("June");
-        break;
-    case 7:
-        Console.WriteLine("July");
-        break;
-    case 8:
-        Console.WriteLine("August");
-        break;
-    case 9:
-        Console.WriteLine("September");
-        break;
-    case 10:
-        Console.WriteLine("October");
-        break;
-    case 11:
-        Console.WriteLine("November");
-        break;
-    case 12:
-        Console.WriteLine("December");
-        break;
-    default:
-        Console.WriteLine("Month not valid");
-        break;
-}
-
-Console.WriteLine("Enter a number 1-500");
-int num = int.Parse(Console.ReadLine());
-
-while (num < 1 || num > 500)
-{
-    if (num < 1)
+    class Program
     {
-        Console.WriteLine("Number too low, try again: ");
-        num = int.Parse(Console.ReadLine());
-    }
-    else if (num > 500)
-    {
-        Console.WriteLine("Number too high, try again: ");
-        num = int.Parse(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fuck you nigga.");
+            Console.WriteLine("Fuck me nigga.");
+            Console.Beep();
+        }
     }
 }
-
-Console.WriteLine($"You picked: {num}");
-
-string[] jacksonFive = {"Michael", "Jackie", "Tito", "Marlin", "Jermaine"};
-
-for (int i = 0; i < jacksonFive.Length; i++)
-{
-    Console.WriteLine(jacksonFive[i]);
-}
-
-int AddTwoNumbers(int a, int b)
-{
-    return a + b;
-}
-
-int nuuum = AddTwoNumbers(1, 2);
-
-Console.WriteLine(nuuum);
-
- */
-
-int amount = 6500;
-int months = 12;
-int bonus = 7000; 
-
-int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
-
-Console.WriteLine($"Yearly wage: {yearlyWage}");
-
-static void FuckMeNigga(string name) {
-    Console.WriteLine($"Fuck me nigga fuck meeee fuck me {name}");
-}
-
-FuckMeNigga("Kai");
-
-int nearlyWage = Utilities.CalculateYearlyWage(amount, months, bonus);
-
-Console.WriteLine(nearlyWage);
--- Utilities.cs
+++ Utilities.cs
@@ -7,8 +7,18 @@ using System.Threading.Tasks;
 namespace FirstCSProj
 {
     internal class Utilities
-    {
-        public static int CalculateYearlyWage(int monthlyWage, int numbersOfMonthsWorked)
+    { 
+        public static void UsingOptionalParameters()
+        {
+            int monthlyWage1 = 1234;
+            int months1 = 12;
+
+            int yearlyWageForEmployee1 = CalculateYearlyWageWithOptional(monthlyWage1, months1, 6000);
+            Console.WriteLine($"Yearly wage for employee (Alex): {yearlyWageForEmployee1}");
+        }
+
+    
+        public static int CalculateYearlyWageWithOptional(int monthlyWage, int numbersOfMonthsWorked, int bonus = 0)
         {
             //Console.WriteLine($"Yearly wage: {monthlyWage * numbersOfMonthsWorked}");
             if (numbersOfMonthsWorked == 12)
@@ -19,7 +29,13 @@ namespace FirstCSProj
             return monthlyWage * numbersOfMonthsWorked;
         }
 
-        public static int CalculateYearlyWage(int monthlyWage, int numbersOfMonthsWorked, int bonus)
+        public static int CalculateYearlyWage(int monthlyWage, int numbersOfMonthsWorked)
+        {
+            //Console.WriteLine($"Yearly wage: {monthlyWage * numbersOfMonthsWorked}");
+
+            return monthlyWage * numbersOfMonthsWorked;
+        }
+        public static int CalculateYearlyWage(int monthlyWage, int numbersOfMonthsWorked, int bonus = 0)
         {
             //Console.WriteLine($"Yearly wage: {monthlyWage * numbersOfMonthsWorked}");
             if (numbersOfMonthsWorked == 12)
