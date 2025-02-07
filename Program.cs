9e161d4c17c2355645d22d413ffdc94ed36618f8
Kai <kharimobley@gmail.com>
2025-02-07 01:47:33 -0500

    Comments lol

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ namespace FirstCSProj
     {
         static void Main(string[] args)
         {
+            Console.Write("Fuck");
             Console.WriteLine("Fuck you nigga.");
             Console.WriteLine("Fuck me nigga.");
             Console.Beep();
+
+            // This is a comment lol
         }
     }
 }
