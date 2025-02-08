176c17ef9a6d5978b6e3b204a0bbadd1df04ad72
Kai <kharimobley@gmail.com>
2025-02-07 22:16:06 -0500

    meh

--- Program.cs
+++ Program.cs
@@ -6,11 +6,14 @@ namespace FirstCSProj
     {
         static void Main(string[] args)
         {
-            Console.Write("Fuck");
+            Console.Write("Fuck\nYounigga");
             Console.WriteLine("Fuck you nigga.");
             Console.WriteLine("Fuck me nigga.");
             Console.Beep();
+            Console.ReadKey();
 
+            int x; // declaration
+            x = 123; // initialization
             // This is a comment lol
         }
     }
