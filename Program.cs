9846325406e9d4167f58fff62d9c02204c1336e8
Kai <kharimobley@gmail.com>
2025-05-25 15:35:03 -0400

    Fix Program.cs

--- Program.cs
+++ Program.cs
@@ -4,26 +4,6 @@ namespace FirstCSProj
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Human jackie = new Human("Jackie", 25);
-            jackie.Eat();
-
-            Human tito = new Human("Tito", 20);
-            tito.Sleep();
-
-            Human jermaine = new Human("Jermaine", 17);
-            jermaine.Eat();
-            jermaine.Sleep();
-
-            Human marlon = new Human("Marlon", 15);
-            marlopn.sleep;
-
-            Ninja michael = new Ninja("Michael", "Spin-Kick", "Kunai", 5);
-            michael.Eat();
-            michael.UseSkill();
-
-            Human.HowManyPeople();
-        }
+        
     }
 }
